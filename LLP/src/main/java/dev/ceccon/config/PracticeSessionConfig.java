5c6d0565b9bf2b1264565199ec26a02a44b48fd7
Tiago Ceccon <ceccon.t@hotmail.com>
2025-03-10 23:48:00 -0300

    Initialize PracticeSessionConfig with default practice session

--- LLP/src/main/java/dev/ceccon/config/PracticeSessionConfig.java
+++ LLP/src/main/java/dev/ceccon/config/PracticeSessionConfig.java
@@ -1,6 +1,14 @@
 package dev.ceccon.config;
 
 import dev.ceccon.practice.Language;
+import dev.ceccon.practice.PracticeCharacter;
+import dev.ceccon.practice.PracticeScene;
+import dev.ceccon.practice.PracticeSession;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
 
 public class PracticeSessionConfig {
 
@@ -8,10 +16,14 @@ public class PracticeSessionConfig {
     private static final Object instanceLock = new Object();
 
     private Language practicedLanguage = Language.FRENCH;
+    private PracticeSession practiceSession;
 
     public static PracticeSessionConfig getInstance() {
         synchronized (instanceLock) {
-            if (instance == null) instance = new PracticeSessionConfig();
+            if (instance == null) {
+                instance = new PracticeSessionConfig();
+                instance.practiceSession = getDefaultSession();
+            }
         }
         return instance;
     }
@@ -24,4 +36,49 @@ public class PracticeSessionConfig {
         return practicedLanguage;
     }
 
+    public PracticeSession getPracticeSession() {
+        return practiceSession;
+    }
+
+    public void setPracticeSession(PracticeSession practiceSession) {
+        this.practiceSession = practiceSession;
+    }
+
+    private static PracticeSession getDefaultSession() {
+        PracticeScene defaultScene = loadDefaultScene();
+
+        String scenario = defaultScene.getScenario();
+
+        byte[] aiCharacterImageBytes;
+        try {
+            File aiCharacterImageFile = new File(PracticeSessionConfig.class.getClassLoader().getResource(defaultScene.getAiCharacterImagePath()).toURI());
+            aiCharacterImageBytes = Files.readAllBytes(aiCharacterImageFile.toPath());
+        } catch (URISyntaxException | IOException e) {
+            System.out.println("Could not load default AI character image.");
+            throw new RuntimeException(e);
+        }
+
+        byte[] humanCharacterImageBytes;
+        try {
+            File humanCharacterImageFile = new File(PracticeSessionConfig.class.getClassLoader().getResource(defaultScene.getHumanCharacterImagePath()).toURI());
+            humanCharacterImageBytes = Files.readAllBytes(humanCharacterImageFile.toPath());
+        } catch (URISyntaxException | IOException e) {
+            System.out.println("Could not load default human character image.");
+            throw new RuntimeException(e);
+        }
+
+        PracticeCharacter aiCharacter = new PracticeCharacter(defaultScene.getAiCharacterName(), defaultScene.getAiCharacterBio(), aiCharacterImageBytes);
+        PracticeCharacter humanCharacter = new PracticeCharacter(defaultScene.getHumanCharacterName(), defaultScene.getHumanCharacterBio(), humanCharacterImageBytes);
+
+        return new PracticeSession(scenario, aiCharacter, humanCharacter);
+    }
+
+    private static PracticeScene loadDefaultScene() {
+        try {
+            return PracticeScene.fromFile(new File(PracticeSessionConfig.class.getClassLoader().getResource("scenes/default.practice").toURI()));
+        } catch (URISyntaxException e) {
+            System.out.println("Could not load default scene.");
+            throw new RuntimeException(e);
+        }
+    }
 }
--- LLP/src/test/java/dev/ceccon/config/PracticeSessionConfigTest.java
+++ LLP/src/test/java/dev/ceccon/config/PracticeSessionConfigTest.java
@@ -1,6 +1,7 @@
 package dev.ceccon.config;
 
 import dev.ceccon.practice.Language;
+import dev.ceccon.practice.PracticeSession;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +13,24 @@ class PracticeSessionConfigTest {
         PracticeSessionConfig config = PracticeSessionConfig.getInstance();
 
         assertNotNull(config);
+    }
+
+    @Test
+    void initializesWithDefaultSession() {
+        String defaultScenario = "In the middle of a modern city, the robot AIBot is waiting for a bus when another robot, User, approaches it.";
+        String defaultAiCharacterName = "AIBot";
+        String defaultAiCharacterDescription = "AIBot is a friendly and talkative robot.";
+        String defaultHumanCharacterName = "User";
+        String defaultHumanCharacterDescription = "User is a robot who is traveling the world, and has just arrived in this country.";
+
+        PracticeSessionConfig config = PracticeSessionConfig.getInstance();
+        PracticeSession session = config.getPracticeSession();
+
+        assertEquals(defaultScenario, session.getScenario());
+        assertEquals(defaultAiCharacterName, session.getAiCharacter().getName());
+        assertEquals(defaultAiCharacterDescription, session.getAiCharacter().getDescription());
+        assertEquals(defaultHumanCharacterName, session.getHumanCharacter().getName());
+        assertEquals(defaultHumanCharacterDescription, session.getHumanCharacter().getDescription());
     }
 
     @Test

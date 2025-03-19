23272452a20e7b63000695e8e4a9c9191280abd6
Tiago Ceccon <ceccon.t@hotmail.com>
2025-03-19 01:20:12 -0300

    Add support for command line option to set LLM port

--- LLP/src/main/java/dev/ceccon/LLP.java
+++ LLP/src/main/java/dev/ceccon/LLP.java
@@ -1,5 +1,10 @@
 package dev.ceccon;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import dev.ceccon.config.LLMAPIConfig;
+import dev.ceccon.conversation.Chat;
 import dev.ceccon.gui.MainView;
 
 import javax.swing.*;
@@ -11,5 +16,42 @@ public class LLP {
         SwingUtilities.invokeLater(() -> {
             new MainView();
         });
+    }
+
+    public static void parseArguments(String[] args, LLMAPIConfig apiConfig) throws IllegalArgumentException {
+        LLPArgs llpArgs = new LLPArgs();
+        JCommander cmd = JCommander.newBuilder()
+                .addObject(llpArgs)
+                .build();
+
+        try {
+            cmd.parse(args);
+        } catch(ParameterException e) {
+            throw new IllegalArgumentException("Could not parse parameter.");
+        }
+
+        if (llpArgs.hasPort()) {
+            Integer port = llpArgs.getPort();
+            apiConfig.setPort(port.toString());
+        }
+
+    }
+
+    static class LLPArgs {
+        @Parameter(
+                names = {"-p", "--port"},
+                description = "LLM server port",
+                required = false,
+                arity = 1
+        )
+        private Integer port;
+
+        public Integer getPort() {
+            return port;
+        }
+
+        public boolean hasPort() {
+            return port != null;
+        }
     }
 }
--- LLP/src/test/java/dev/ceccon/LLPTest.java
+++ LLP/src/test/java/dev/ceccon/LLPTest.java
@@ -1,4 +1,41 @@
 package dev.ceccon;
 
+import dev.ceccon.config.LLMAPIConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LLPTest {
-}
+    @Test
+    void cliOptionPortWithoutValueThrowsException() {
+        String[] args = new String[]{"-p"};
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            LLP.parseArguments(args, new LLMAPIConfig());
+        });
+    }
+
+    @Test
+    void cliOptionPortWithNonNumericValueThrowsException() {
+        String port = "port";
+        String[] args = new String[]{"-p", port};
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            LLP.parseArguments(args, new LLMAPIConfig());
+        });
+    }
+
+    @Test
+    void cliOptionPortWithNumericValueSetsPortParameterOnApiConfig() {
+        String port = "8089";
+        String[] args = new String[]{"-p", port};
+
+        LLMAPIConfig apiConfig = new LLMAPIConfig();
+
+        LLP.parseArguments(args, apiConfig);
+
+        String portOnApiConfig = apiConfig.getPort();
+
+        assertEquals(port, portOnApiConfig);
+    }}
